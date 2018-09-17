baeb242d34ad666f0774910ab776335a76e0e9e9
Tino Sabetta <tino.sabetta@gmail.com>
2018-09-17 08:33:52 +0200

    started to work on matching function

--- js/memory.js
+++ js/memory.js
@@ -1,13 +1,21 @@
 var Grid = document.querySelector("#GameGrid");
 
-Grid.addEventListener("click", test);
+Grid.addEventListener("click", selectCards);
 
-function test(e) {
+function selectCards(e) {
     var clicked = e.target;
     var selection = document.querySelectorAll(".selected");
     if (selection.length < 2) {
         if (clicked.id !== "GameGrid" && clicked.parentElement.classList.contains("selected") === false) {
             clicked.parentElement.classList.toggle("selected");
         }
+    }
+    setTimeout(match, 1000);
+}
+
+function match() {
+    var selection = document.querySelectorAll(".selected");
+    if (selection.length === 2) {
+        alert("yay");
     }
 }
