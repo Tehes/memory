aebfdc1418e1cdde2a2bbaa44c1de6ce837e00a6
Tino Sabetta <tino.sabetta@gmail.com>
2018-09-19 08:59:43 +0200

    shuffle also working now

--- js/memory.js
+++ js/memory.js
@@ -1,8 +1,28 @@
 var Grid = document.querySelector("#GameGrid");
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
 var Motifs = ["apple", "avocado", "banana", "bell-pepper", "cabbage", "cauliflower", "cherry", "grapes", "kiwi", "orange", "pineapple", "pumpkin", "strawberry", "tomato", "watermelon"];
 
-var Motifs = Motifs.concat(Motifs);
+Motifs = Motifs.concat(Motifs);
+Motifs = shuffle(Motifs);
 
 var Cards = document.querySelectorAll(".card .back");
 
