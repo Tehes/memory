efc576167afc545225f4761d56145d47d72fcce7
Tino Sabetta <tino.sabetta@gmail.com>
2018-09-19 08:52:59 +0200

    added card motifs. match function now working. need to shuffle array in next step.

--- js/memory.js
+++ js/memory.js
@@ -1,5 +1,17 @@
 var Grid = document.querySelector("#GameGrid");
 
+var Motifs = ["apple", "avocado", "banana", "bell-pepper", "cabbage", "cauliflower", "cherry", "grapes", "kiwi", "orange", "pineapple", "pumpkin", "strawberry", "tomato", "watermelon"];
+
+var Motifs = Motifs.concat(Motifs);
+
+var Cards = document.querySelectorAll(".card .back");
+
+for (var i = 0; i < Motifs.length; i++) {
+    Cards[i].style.backgroundImage = "url('assets/"+ Motifs[i] +".svg')";
+    Cards[i].parentElement.dataset.name = Motifs[i];
+}
+
+
 Grid.addEventListener("click", selectCards);
 
 function selectCards(e) {
@@ -14,13 +26,17 @@ function selectCards(e) {
     if (selection.length === 2) {
         setTimeout(function() {
             match(selection);
-        }, 1000)
+        }, 1000);
     }
 }
 
 function match(sel) {
     var selection = document.querySelectorAll(".selected");
     if (selection.length === 2) {
+        if (sel[0].dataset.name === sel[1].dataset.name) {
+            sel[0].classList.add("matched");
+            sel[1].classList.add("matched");
+        }
         sel[1].classList.remove("selected");
         sel[0].classList.remove("selected");
     }
